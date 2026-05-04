5a7734cdf57fc28e0adb2de7bcdeb675d289d7db
EncryptedCicada <dev.varnit@gmail.com>
2026-05-04 15:20:59 -0700

    fix: add deps for west

--- shells/zephyr.nix
+++ shells/zephyr.nix
@@ -20,14 +20,67 @@ pkgs.mkShell {
     # Flashing
     dfu-util
 
+    # Devtools
+    gitlint
+
     # Zephyr tooling (using the system python for consistency)
     (python3.withPackages (
       ps: with ps; [
         west
         pyelftools
+        pyyaml
         jsonschema
+        pykwalify
+        canopen
+        packaging
+        patool
+        pylink-square
+        requests
+        semver
+        tqdm
+        reuse
+        anytree
+        intelhex
+        colorama
+        ply
+        gcovr
+        coverage
+        pytest
+        mypy
+        junitparser
+        python-dotenv
+        gitpython
+        plotly
+        junit2html
+        lpc-checksum
+        spsdk
+        pillow
+        pygithub
+        graphviz
         pyusb
         psutil
+        pyserial
+        packaging
+        setuptools
+        lxml
+        pylint
+        ruff
+        sphinx-lint
+        tabulate
+        unidiff
+        vermin
+        yamllint
+        pyocd
+        natsort
+        cbor
+        python-can
+        spdx-tools
+        opencv-python
+        numpy
+        python-dateutil
+        docopt
+        ruamel-yaml
+        six
       ]
     ))
 
